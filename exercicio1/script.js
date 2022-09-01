1a6c58fce3ee03ec32f6e06c96db413e35dd7141
VeltoPD <veltopd@gmail.com>
2022-09-01 18:01:22 -0300

    Entrega dos exercícios a010-arrays

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+const numeros = [4,50,79,88]
+const strings = ["perseverança","resiliência","Determinação","Objetivo"]
+const mista = ["Vitória",100,true,"Desistir",false,-100]
+
+console.log(numeros,strings,mista)
+
